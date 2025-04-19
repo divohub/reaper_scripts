47264b1ea86abb4dafff5ea83306aeb76dda28d3
Fedor Kupryakov <hihi@divosound.com>
2025-04-19 20:28:36 +0300

    made a reaper task test

--- reaper_test_script.lua
+++ reaper_test_script.lua
@@ -1,10 +1,12 @@
 
 -- At the start of the script copy this:
 -- Instead of " reaper.GetResourcePath() ..'Scripts/Daniel Lumertz Scripts/LUA Socket/socket module/' " put the path for your socket module folder. The folder that hold all lua modules files AND the "socket" folder
-package.cpath = package.cpath .. ";" .. reaper.GetResourcePath() ..'/Scripts/Lua Sockets/socket module/?.dll'    -- WINDOWS ONLY: Add socket module path for .dll files
+--[[package.cpath = package.cpath .. ";" .. reaper.GetResourcePath() ..'/Scripts/Lua Sockets/socket module/?.dll'    -- WINDOWS ONLY: Add socket module path for .dll files
 package.path = package.path .. ";" .. reaper.GetResourcePath()   ..'/Scripts/Lua Sockets/socket module/?.lua'      -- Add all lua socket modules to the path  
+]]--require("mobdebug").start() -- Start mobdebug module
 
-require("mobdebug").start() -- Start mobdebug module
 
-local track = reaper.GetTrack(0, 0)
-print("hello world")
+---@diagnostic disable-next-line: undefined-global
+reaper.ShowConsoleMsg("hello world")
+
+
